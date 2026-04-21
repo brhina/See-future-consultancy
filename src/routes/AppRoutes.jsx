add4686739da342e83a43a106d0357f79c5ae1e6
brieuser <brwubet@gmail.com>
2026-04-21 08:16:02 +0300

    scroll to top functionality

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence, motion as Motion, useReducedMotion } from 'framer-motion'
 import { pageVariants } from '../utils/motionPresets'
+import ScrollToTop from '../components/ScrollToTop'
 
 import Home from '../pages/Home';
 import About from '../pages/About';
@@ -17,28 +18,32 @@ function AppRoutes() {
   const reduceMotion = useReducedMotion()
 
   return (
-    <AnimatePresence mode="wait">
-      <Motion.main
-        key={location.pathname}
-        className="min-h-screen w-full"
-        variants={reduceMotion ? undefined : pageVariants}
-        initial={reduceMotion ? false : 'initial'}
-        animate={reduceMotion ? undefined : 'animate'}
-        exit={reduceMotion ? undefined : 'exit'}
-      >
-        <Routes location={location}>
-          <Route path="/" element={<Home />} />
-          <Route path="/latest-news" element={<LatestNews />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/thematic-areas" element={<ThematicAreas />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/terms" element={<TermsOfService />} />
-          <Route path="/privacy" element={<PrivacyPolicy />} />
-          <Route path="/help" element={<HelpCenter />} />
-        </Routes>
-      </Motion.main>
-    </AnimatePresence>
+    <>
+      <ScrollToTop />
+
+      <AnimatePresence mode="wait">
+        <Motion.main
+          key={location.pathname}
+          className="min-h-screen w-full"
+          variants={reduceMotion ? undefined : pageVariants}
+          initial={reduceMotion ? false : 'initial'}
+          animate={reduceMotion ? undefined : 'animate'}
+          exit={reduceMotion ? undefined : 'exit'}
+        >
+          <Routes location={location}>
+            <Route path="/" element={<Home />} />
+            <Route path="/latest-news" element={<LatestNews />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/thematic-areas" element={<ThematicAreas />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/terms" element={<TermsOfService />} />
+            <Route path="/privacy" element={<PrivacyPolicy />} />
+            <Route path="/help" element={<HelpCenter />} />
+          </Routes>
+        </Motion.main>
+      </AnimatePresence>
+    </>
   )
 }
 
